41e76b82898e5a797e168fcdbdf9ab241cb40215
whitegfx <petr.votocek@gfx.cz>
2013-05-17 21:22:09 +0200

    wide to narrow using mbstowcs
    wide to narrow using mbstowcs

--- proj.ios/EziLocalizationTest/EziLocalizationTest/ios/IOSLocalizationTest.mm
+++ proj.ios/EziLocalizationTest/EziLocalizationTest/ios/IOSLocalizationTest.mm
@@ -40,8 +40,27 @@ void LocalizationWrapper::getUserDetails(LocalizationWrapper::UserDetailCallback
 {
     // We have write our test here.
     
-    std::wstring testString = L"dßdýdうd⬅";
+    //std::wstring testString = L"dßdýdうd⬅";
+    NSString* specialString = [NSString stringWithFormat:@"dßdýdうd⬅", nil];
     
+    const char *cString = [specialString cStringUsingEncoding:NSUTF8StringEncoding];
+    char * oldlocaleinfo = setlocale(LC_CTYPE, "");
+    char * localeinfo = setlocale(LC_CTYPE, "C");//UTF-8
+    
+    printf ("Locale is: %s\n", localeinfo );
+    wchar_t *outStr = NULL;
+    size_t size = mbstowcs(NULL, cString, 0);
+    outStr = new wchar_t[size + 1];
+    if (outStr) {
+        memset(outStr, 0, size * sizeof(wchar_t));
+        size_t ret = mbstowcs(outStr, cString, size+1);
+        if (ret == -1) {
+            delete[] outStr;
+            outStr = NULL;
+        }
+    }
+    localeinfo = setlocale(LC_CTYPE, oldlocaleinfo);
+    std::wstring testString(outStr);
     if (mUserCallback)
     {
         (mUserCallback)(testString);
